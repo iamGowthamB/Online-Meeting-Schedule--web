8a2ef905a08c16ea2fe7cd841a4481466d9fc7e1
iamGowthamB <150770038+iamGowthamB@users.noreply.github.com>
2024-11-13 20:18:21 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,35 +1,122 @@
-import { Routes, Route } from "react-router-dom"
-import LoginForm from "./Components/LoginForm/LoginForm";
-import SignUp from "./Components/SignUp/SignUp";
+// import { Routes, Route } from "react-router-dom"
+// import LoginForm from "./Components/LoginForm";
+// import SignUp from "./Components/SignUp";
+// import Header from "./Header";
+// import MainPage from "./MainPage";
+// import Home from "./Home";
+
+
+// function App() {
+//   return (
+//     <div className="App">
+    
+
+//      {/* <Home /> */}
+//       <Header />
+//       <Routes>
+//         <Route path="/" element={ <LoginForm/> } />
+//         <Route path="signup" element={ <SignUp /> } />
+//         <Route path="login" element={ <LoginForm /> } />
+//         <Route path="home" element={ <Home/> } />
+//         <Route path="mainpage" element={ <MainPage/> } />
+
+       
+//       </Routes> 
+      
+    
+//     </div>
+//   );
+// }
+
+// export default App;
+import React from 'react';
+import { MeetingProvider } from './MeetingContext';
+import { Routes, Route } from "react-router-dom";
+import LoginForm from "./Components/LoginForm";
+import SignUp from "./Components/SignUp";
 import Header from "./Header";
 import MainPage from "./MainPage";
 import Home from "./Home";
 
 
 
-
-
 function App() {
   return (
-    <div className="App">
-    
-
-     {/* <Home /> */}
-      <Header />
-      <Routes>
-        <Route path="/" element={ <LoginForm/> } />
-        <Route path="signup" element={ <SignUp /> } />
-        <Route path="login" element={ <LoginForm /> } />
-        <Route path="home" element={ <Home/> } />
-        <Route path="mainpage" element={ <MainPage/> } />
-
-       
-      </Routes> 
+    <MeetingProvider>
       
-    
-    </div>
+      <div className="App">
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route path="login" element={<LoginForm />} />
+          <Route path="home" element={<Home />} />
+          <Route path="mainpage" element={<MainPage />} />
+        </Routes>
+
+      </div>
+
+    </MeetingProvider>
   );
 }
 
 export default App;
 
+// import React from 'react';
+// import { MeetingProvider } from './MeetingContext';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import LoginForm from './Components/LoginForm';
+// import SignUp from './Components/SignUp';
+// import Header from './Header';
+// import MainPage from './MainPage';
+// import Home from './Home';
+
+// function App() {
+//   return (
+//     <MeetingProvider>
+//       <Router>
+//         <div className="App">
+//           <Header />
+//           <Routes>
+//             <Route path="/" element={<LoginForm />} />
+//             <Route path="signup" element={<SignUp />} />
+//             <Route path="login" element={<LoginForm />} />
+//             <Route path="home" element={<Home />} />
+//             <Route path="mainpage" element={<MainPage />} />
+//           </Routes>
+//         </div>
+//       </Router>
+//     </MeetingProvider>
+//   );
+// }
+
+// export default App;
+
+// import React from 'react';
+// import { MeetingProvider } from './MeetingContext';
+// import { Routes, Route } from 'react-router-dom';
+// import LoginForm from './Components/LoginForm';
+// import SignUp from './Components/SignUp';
+// import Header from './Header';
+// import MainPage from './MainPage';
+// import Home from './Home';
+
+// function App() {
+//   return (
+//     <MeetingProvider>
+//       <div className="App">
+//         <Header />
+//         <Routes>
+//           <Route path="/" element={<LoginForm />} />
+//           <Route path="signup" element={<SignUp />} />
+//           <Route path="login" element={<LoginForm />} />
+//           <Route path="home" element={<Home />} />
+//           <Route path="mainpage" element={<MainPage />} />
+//         </Routes>
+//       </div>
+//     </MeetingProvider>
+//   );
+// }
+
+// export default App;
